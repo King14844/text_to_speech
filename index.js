210da37f181d1772e0b82e1a4a60770f1a122cdd
Vinay Singirikonda <kingking148414@gmail.com>
2023-11-23 17:06:04 +0530

    c

---
+++ index.js
@@ -0,0 +1,45 @@
+const sdk = require("microsoft-cognitiveservices-speech-sdk");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const subscriptionKey = "3c4121fca5c9472db7c84fc3e4442c0f";
+const region = "eastus";
+
+const speechConfig = sdk.SpeechConfig.fromSubscription(subscriptionKey, region);
+speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural";
+const synthesizer = new sdk.SpeechSynthesizer(speechConfig, null);
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/synthesize', (req, res) => {
+    const { text } = req.body;
+    const visemeData = [];
+    synthesizer.visemeReceived = function (s, e) {
+        visemeData.push({
+            audioOffset: Math.floor(e.audioOffset / 10000),
+            visemeId: e.visemeId,
+        });
+    };
+
+    if (text) {
+        synthesizer.speakTextAsync(text, function (result) {
+            if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+                console.log("Synthesis finished.");
+                const audioData = Buffer.from(result.audioData).toString('base64');
+                res.json({ audioData, visemeData });
+            } else {
+                console.error(`Error synthesizing audio: ${result.reason}`);
+                res.status(500).json({ error: "Error synthesizing audio" });
+            }
+        });
+    } else {
+        res.status(400).json({ error: 'Text not provided' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
